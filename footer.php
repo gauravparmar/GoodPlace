7b9cc39d38875adafc380a2b01f141855a83c350
System Administrator <root@dongy-Mac-Mini.local>
2013-03-13 15:45:10 +0800

    a

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Good Place
+ * @since Good Place 0.1
+ */
+?>
+	</div>
+	<div class='bottom-nav'>
+		<div class='other-links'></div>
+		<hr>
+		<div class='resources'></div>
+	</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Good Place
+ * @since Good Place 0.1
+ */
+?><!doctype html>
+<html>
+<head>
+<link href='http://fonts.googleapis.com/css?family=Geo:400,400italic' rel='stylesheet' type='text/css'>
+<link href='<?php bloginfo('stylesheet_url') ?>' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class='top-nav'></div>
+	<div class='wrapper'>
